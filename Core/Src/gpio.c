e51e656cf76f418e57dc8029c1b74b55f1cdfd4f
Joe <bug705076@gmail.com>
2022-12-04 12:32:40 +0800

    add SWD pin to CubeMX pinout modify DEBUG optimization level to -O0

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -33,6 +33,8 @@
 /* USER CODE END 1 */
 
 /** Configure pins
+     PA13 (JTMS/SWDIO)   ------> DEBUG_JTMS-SWDIO
+     PA14 (JTCK/SWCLK)   ------> DEBUG_JTCK-SWCLK
 */
 void MX_GPIO_Init(void)
 {
@@ -41,6 +43,7 @@ void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
